6d9ea1f38e6b4655f932cefed61d3df33490eacb
rud-y <rudolf.zachar@gmail.com>
2020-12-21 10:52:05 +0000

    'Show' links for individual boxes-projects

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-const  output = () => {
-    const text = document.getElementById('output-text');
-    text.textContent = "Hi there!";
-    return text;
+var showText = function (target, message, index, interval) {   
+    const message = "AHOJ";
+  if (index < message.length) {
+    $(target).append(message[index++]);
+    setTimeout(function () { showText(target, message, index, interval); }, interval);
+  }
 }
+
+$(function () {
+  showText(document.getElementById('output'), "Hello, World!", 0, 500);   
+});
