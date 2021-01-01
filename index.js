875c48560d33eb4264ffac8246e2d33914dc163e
rud-y <rudolf.zachar@gmail.com>
2021-01-01 13:24:07 +0000

    Header section simplify elem, logo animation

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-var showText = function (target, message, index, interval) {   
-    const message = "AHOJ";
-  if (index < message.length) {
-    $(target).append(message[index++]);
-    setTimeout(function () { showText(target, message, index, interval); }, interval);
-  }
-}
+// var showText = function (target, message, index, interval) {   
+//     const message = "AHOJ";
+//   if (index < message.length) {
+//     $(target).append(message[index++]);
+//     setTimeout(function () { showText(target, message, index, interval); }, interval);
+//   }
+// }
 
-$(function () {
-  showText(document.getElementById('output'), "Hello, World!", 0, 500);   
+// $(function () {
+//   showText(document.getElementById('output'), "Hello, World!", 0, 500);   
+// });
+
+
+
+document.window.scroll(function(e) {
+  var ul = document.getElementById('ul-nav');
+  var isPositionFixed = ul.style="position:fixed;";
+  if (document.window.scrollTop() > 200 && !isPositionFixed) {
+    ul.style = "position:fixed; top:0px;";
+  }
+  if (document.window.scrollTop() < 200 && isPositionFixed) {
+    ul.style = "position:relative; top:0px;";
+  }
 });
