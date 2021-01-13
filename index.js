6c552f9ae5ce454527126575e227d048384716ba
rud-y <rudolf.zachar@gmail.com>
2021-01-13 22:38:43 +0000

    Added project listings

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+
+// fetch('https://restcountries.eu/rest/v2/all')
+// .then(res => res.json())
+// .then(data => countries = data)
+// .then(() => console.log(countries))
+
+
 // var showText = function (target, message, index, interval) {   
 //     const message = "AHOJ";
 //   if (index < message.length) {
@@ -12,13 +19,13 @@
 
 
 
-document.window.scroll(function(e) {
-  var ul = document.getElementById('ul-nav');
-  var isPositionFixed = ul.style="position:fixed;";
-  if (document.window.scrollTop() > 200 && !isPositionFixed) {
-    ul.style = "position:fixed; top:0px;";
-  }
-  if (document.window.scrollTop() < 200 && isPositionFixed) {
-    ul.style = "position:relative; top:0px;";
-  }
-});
+// document.window.scroll(function(e) {
+//   var ul = document.getElementById('ul-nav');
+//   var isPositionFixed = ul.style="position:fixed;";
+//   if (document.window.scrollTop() > 200 && !isPositionFixed) {
+//     ul.style = "position:fixed; top:0px;";
+//   }
+//   if (document.window.scrollTop() < 200 && isPositionFixed) {
+//     ul.style = "position:relative; top:0px;";
+//   }
+// });
