c1ec46236737f94e92fb09c76992ff4e80d33a9c
rud-y <rudolf.zachar@gmail.com>
2021-01-19 16:30:04 +0000

    Images addded to tournament dir

--- index.js
+++ index.js
@@ -1,24 +1,3 @@
-
-// fetch('https://restcountries.eu/rest/v2/all')
-// .then(res => res.json())
-// .then(data => countries = data)
-// .then(() => console.log(countries))
-
-
-// var showText = function (target, message, index, interval) {   
-//     const message = "AHOJ";
-//   if (index < message.length) {
-//     $(target).append(message[index++]);
-//     setTimeout(function () { showText(target, message, index, interval); }, interval);
-//   }
-// }
-
-// $(function () {
-//   showText(document.getElementById('output'), "Hello, World!", 0, 500);   
-// });
-
-
-
 // document.window.scroll(function(e) {
 //   var ul = document.getElementById('ul-nav');
 //   var isPositionFixed = ul.style="position:fixed;";
