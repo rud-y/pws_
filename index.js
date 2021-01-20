87bfa977df045aa2725f933d81f21bf27a8765be
rud-y <rudolf.zachar@gmail.com>
2021-01-20 09:28:44 +0000

    Dashboard cards layout

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-// document.window.scroll(function(e) {
-//   var ul = document.getElementById('ul-nav');
-//   var isPositionFixed = ul.style="position:fixed;";
+// document.window.scroll(function (e) {
+//   var ul = document.getElementById("ul-nav");
+//   var isPositionFixed = (ul.style = "position:fixed;");
 //   if (document.window.scrollTop() > 200 && !isPositionFixed) {
 //     ul.style = "position:fixed; top:0px;";
 //   }
