e699604253236f3039b245398035a899101f9916
rud-y <rudolf.zachar@gmail.com>
2021-01-30 18:34:52 +0000

    Modified some elements, backgroundImage,..

--- index.js
+++ index.js
@@ -8,3 +8,23 @@
 //     ul.style = "position:relative; top:0px;";
 //   }
 // });
+window.addEventListener("DOMContentLoaded", () => {
+  const myDiv = document.querySelector("#logo-div");
+  let pics = [
+    "images/PANO_holyrood.jpg",
+    "images/PANO_scotland.jpg",
+    // "images/edinburgh_green.jpg",
+    // "images/edi1.jpg",
+    // "images/pent1.jpg",
+    // "images/pent2.jpg",
+    // "images/pepnt3.jpg",
+  ];
+  // const timerId = setInterval(() => {
+  //   console.log(pics.length);
+  //   let x = Math.floor(Math.random() * pics.length);
+  //   myDiv.style.backgroundImage = "url('" + pics[x] + "')";
+  //   pic[x].style.objectFit = "contain";
+  //   // myDiv.style.verticalAlign = "middle";
+  //   // myDiv.style.backgroundSize = "100%";
+  // }, 2000);
+});
