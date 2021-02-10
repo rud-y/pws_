3b7fb873987e9779f3d4abc975789e76d2e25c93
rud-y <rudolf.zachar@gmail.com>
2021-02-10 15:58:15 +0000

    sections style modification, background from intr moved to bottom contact info, decriptions

--- index.js
+++ index.js
@@ -8,23 +8,23 @@
 //     ul.style = "position:relative; top:0px;";
 //   }
 // });
-window.addEventListener("DOMContentLoaded", () => {
-  const myDiv = document.querySelector("#logo-div");
+document.addEventListener("DOMContentLoaded", () => {
+  const myDiv = document.querySelector("#contact");
   let pics = [
     "images/PANO_holyrood.jpg",
     "images/PANO_scotland.jpg",
-    // "images/edinburgh_green.jpg",
-    // "images/edi1.jpg",
+    "images/edinburgh_green.jpg",
+    "images/edi1.jpg",
     // "images/pent1.jpg",
     // "images/pent2.jpg",
     // "images/pepnt3.jpg",
   ];
-  // const timerId = setInterval(() => {
-  //   console.log(pics.length);
-  //   let x = Math.floor(Math.random() * pics.length);
-  //   myDiv.style.backgroundImage = "url('" + pics[x] + "')";
-  //   pic[x].style.objectFit = "contain";
-  //   // myDiv.style.verticalAlign = "middle";
-  //   // myDiv.style.backgroundSize = "100%";
-  // }, 2000);
+
+  setInterval(() => {
+    console.log(pics.length);
+    let x = Math.floor(Math.random() * pics.length);
+    myDiv.style.backgroundImage = "url('" + pics[x] + "')";
+    // pics[x].style.objectFit = "contain";
+  }, 5000);
+  // console.log(picRotate);
 });
