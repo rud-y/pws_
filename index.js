f0d408836ee1c815d2ad8542e2cf8ca65e62981f
rud-y <rudolf.zachar@gmail.com>
2021-03-11 14:59:42 +0000

    images size compressed

--- index.js
+++ index.js
@@ -8,24 +8,21 @@
 //     ul.style = "position:relative; top:0px;";
 //   }
 // });
+
 document.addEventListener("DOMContentLoaded", () => {
   const myDiv = document.querySelector("#contact");
   let pics = [
     "images/PANO_holyrood.jpg",
     "images/PANO_scotland.jpg",
     "images/edinburgh_green.jpg",
-    "images/edi1.jpg",
     "images/pent2.jpg",
     "images/pent1.jpg",
-    // "images/pent1.jpg",
-    // "images/pent2.jpg",
-    // "images/pepnt3.jpg",
+    "images/pent3.jpg",
   ];
 
   setInterval(() => {
-    console.log(pics.length);
     let x = Math.floor(Math.random() * pics.length);
     myDiv.style.backgroundImage = "url('" + pics[x] + "')";
     // pics[x].style.objectFit = "contain";
-  }, 6000);
+  }, 5000);
 });
