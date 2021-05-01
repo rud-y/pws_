b29809c9a7d57f5ddbb26c8208586f7e60ce089f
rud-y <rudolf.zachar@gmail.com>
2021-05-01 17:37:36 +0100

    Added rockpaperscissors,

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-// document.window.scroll(function (e) {
-//   var ul = document.getElementById("ul-nav");
-//   var isPositionFixed = (ul.style = "position:fixed;");
-//   if (document.window.scrollTop() > 200 && !isPositionFixed) {
-//     ul.style = "position:fixed; top:0px;";
-//   }
-//   if (document.window.scrollTop() < 200 && isPositionFixed) {
-//     ul.style = "position:relative; top:0px;";
-//   }
-// });
-
 document.addEventListener("DOMContentLoaded", () => {
   const myDiv = document.querySelector("#contact");
   let pics = [
